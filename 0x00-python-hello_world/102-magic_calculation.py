99f9923b0463fdde4ba4195a7fe3ade64648c7cc
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-05-31 12:35:58 +0100

    Update 102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    """Finds the result of a raised to the power b
-    Add 98 and return the resulting value"""
-    b = a ** b
-    return b + 98
+    return (98 + (a ** b))
