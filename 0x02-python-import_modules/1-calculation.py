7ca418fda794186b06f41047aca633bc87e5fb74
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-06-03 01:05:56 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    print(f"{:d} + {:d} = {:d}"(a, b, add(a, b)))
-    print(f"{:d} - {:d} = {:d}"(a, b, sub(a, b)))
-    print(f"{:d} * {:d} = {:d}"(a, b, mul(a, b)))
-    print(f"{:d} / {:d} = {:d}"(a, b, div(a, b)))
+
+    print(f"{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print(f"{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print(f"{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print(f"{:d} / {:d} = {:d}".format(a, b, div(a, b)))
