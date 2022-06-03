f9de100b9999b048613b9a4d861eba8ca728981f
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-06-03 03:18:04 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
+import sys
+
 if __name__ == "__main__":
     names = dir(hidden_4)
     for name in names:
-        if name[0] != '__':
+        if name[:2] != '__':
             print(name)
