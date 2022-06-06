46cce1aada59f8c46e0b1787843d530f352c0bf5
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-06-07 00:50:52 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-   
-    if sentence == "":
-        return (0, None)
+    if sentence != "":
+        return ((len(sentence), sentence[0]))
     else:
-        return (len(sentence), sentence[0])
+        return ((0, None))
