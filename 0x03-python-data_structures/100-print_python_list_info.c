a968d6d7b12e02c190c733c923bdfaa10f90e0aa
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-06-07 01:28:44 +0100

    Create 100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,18 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints the length of a python list
+ * @p: pointer to a PyObject
+ */
+void print_python_list_info(PyObject *p)
+{
+	size_t i =  0;
+	size_t length = 0;
+	PyListObject *Plist = (PyListObject *)p;
+
+	length = PyList_Size(p);
+	printf("[*] Size of the Python List = %lu\n", length);
+	printf("[*] Allocated = %lu\n", Plist->allocated);
+	for (i = 0; i < length; i++)
+		printf("Element %lu: %s\n", i, Py_TYPE(Plist->ob_item[i])->tp_name);
+}
