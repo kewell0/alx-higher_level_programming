f24d631c2e7303d57dfe832fbea5b7beb55407cd
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-06-29 02:42:45 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.c, t = (magic_string.__dict__.get('c', 0) + 1, ', Holberton')
-    return '{:s}{:s}'.format(t[2:], t * (magic_string.c - 1))
+    setattr(magic_string, "n", getattr(magic_string, "n", 0) + 1)
+    return ("Holberton, " * getattr(magic_string, "n", 0))[:-2]
