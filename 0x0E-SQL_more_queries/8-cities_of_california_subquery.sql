cb5fc77deafc7f050045f9c6ee435b8b52978bde
CALEB <owatahcaleb@gmail.com>
2022-08-10 16:43:34 +0100

    more queries

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- lists all the cities of california that can be found in hbtn_0d_usa
+SELECT `id`, `name`
+FROM `cities`
+WHERE `state_id` IN 
+	(SELECT `id` FROM `states`
+	WHERE `name` = "California")
+ORDER BY `id`;
