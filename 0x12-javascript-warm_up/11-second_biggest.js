9dd6064b66d68a0ed5a811ca2e42dbff1b4395d5
CALEB <owatahcaleb@gmail.com>
2022-09-06 04:44:37 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
-if (process.argv.length === 1) {
+if (process.argv.length < 4) {
   console.log('0');
 } else {
-  const num = process.argv.slice(2).map(x => parseInt(x));
-  num.sort(function (a, b) { return b - a; });
-  console.log(num[1]);
+  const size = process.argv.length;
+  const myInts = [];
+  for (let i = 2; i < size; i++) {
+    myInts[i - 2] = parseInt(process.argv[i]);
+  }
+  myInts.sort(function (a, b) { return b - a; });
+  console.log(myInts[1]);
 }
