1185e77fee6aa9c26db53d59a421697d33aab4e2
CALEB <owatahcaleb@gmail.com>
2022-09-06 02:01:12 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const myVar = process.argv.length;
-if (myVar == 2) {
+if (myVar === 2) {
     console.log('No argument');
-} else if (myVar == 3) {
+} else if (myVar === 3) {
     console.log('Argument found');
 } else {
     console.log('Argument found');
