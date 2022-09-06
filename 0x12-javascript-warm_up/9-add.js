c17d20df062996fd391761a0b972e4b7fa64ce0b
CALEB <owatahcaleb@gmail.com>
2022-09-06 03:53:54 +0100

    Create 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argv1 = parseInt(process.argv[2]);
+const argv2 = parseInt(process.argv[3]);
+
+function add (a, b) {
+    return a + b;
+}
+
+console.log(add(argv1, argv2));
