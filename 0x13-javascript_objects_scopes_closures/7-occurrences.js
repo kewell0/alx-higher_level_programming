04098c1de024fc1c92184480631eb07e266acbc4
Caleb <Owatahcaleb@gmail.com>
2022-09-06 16:38:27 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let count = 0;
-    list.forEach(function (item) {
-      if (item === searchElement) {
-        count++;
-      }
-    });
-    return count;
-  };
+  let count = 0;
+  list.forEach(function (item) {
+    if (item === searchElement) {
+      count++;
+    }
+  });
+  return count;
+};
