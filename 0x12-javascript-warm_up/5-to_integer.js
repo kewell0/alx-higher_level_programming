5fbb8d719111856770daf0dda356070fb90a6103
CALEB <owatahcaleb@gmail.com>
2022-09-06 03:13:45 +0100

    loops

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const myVar = parseInt(process.argv[2]);
-if (myVar === undefined || NaN(myVar)) {
+if (process.argv[2] === undefined || NaN(myVar)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + myVar);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let a = 0; a < args.length; a++) {
+  console.log(args[a]);
+}
