f2ff4751a0555b2b7c9ee63ad109d4639333783b
CALEB <owatahcaleb@gmail.com>
2022-09-06 03:44:32 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let x = process.argv[2];
 x = parseInt(x);
-if (isNaN(x) || process.argv[2] === undefined) {
+if (isNaN(x)) {
     console.log('Missing number of occurrences');
 } else {
   for (let a = 0; a > x; a++) {
