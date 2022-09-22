33617393c09f5aea0e3cbaaaae0bf7e0e09e3b3a
CALEB <owatahcaleb@gmail.com>
2022-09-23 00:34:26 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that sends a GET request to a URL with the header variable X-School-User-Id as 98
-curl -sH 'X-School-User-Id: 98' "$1"
+curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
