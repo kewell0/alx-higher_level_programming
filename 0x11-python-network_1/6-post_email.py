43c768a959fc45a3f979e96a1c4b73689ec11df4
CALEB <owatahcaleb@gmail.com>
2022-09-24 00:42:55 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Sends a POST request to the passed URL with the email as a parameter
-and finally displays the body of the response"""
+and displays the body of the response"""
 import requests
 import sys
 
